734f26b88df31cca633760c6c9b9b34794a37716
User1 <user@example.com>
2020-04-15 20:55:32 -0500

    Update Lab 7

--- trungTranmap.html/trungTranmap.js
+++ trungTranmap.html/trungTranmap.js
@@ -1,18 +1,18 @@
-var trungTranmap = L.map('trungTranmap').setView([32.18, -99.14], 4)
-L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(trungTranmap)
-var nolaMarker = L.marker([30, -90]).addTo(trungTranmap)
-var triangle = L.polygon([
-  [31, -88],
-  [35, -88],
-  [32, -82]
-]).addTo(trungTranmap)
-var polyline = L.polyline([
-  [30, -87],
-  [31, -86],
-  [32, -85],
-  [33, -84],
-  [34, -83]
-]).addTo(trungTranmap)
-labnolatriangle.bindPopup('TrungTranTriangle.')
-labnolaMarker.bindPopup('TrungTranNew Orleans.')
-labpolygonline.bindPopup('TrungTranpolyline')
+ var trungTranmap = L.map('trungTranmap').setView([32.18, -99.14], 4)
+ L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png').addTo(trungTranmap)
+ var nolaMarker = L.marker([30, -90]).addTo(trungTranmap)
+ var triangle = L.polygon([
+   [31, -88],
+   [35, -88],
+   [32, -82]
+ ]).addTo(trungTranmap)
+ var polyline = L.polyline([
+   [30, -87],
+   [31, -86],
+   [32, -85],
+   [33, -84],
+   [34, -83]
+ ]).addTo(trungTranmap)
+ triangle.bindPopup('TrungTran Triangle.')
+ nolaMarker.bindPopup('TrungTran New Orleans.')
+ polyline.bindPopup('TrungTran Line.')
